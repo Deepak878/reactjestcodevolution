4ee529b6757a24c90eb97f0a60b1b9bff01b88eb
deepak878 <deepakbhattarai878@gmail.com>
2023-05-14 08:26:08 +0545

    Grouping test

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,14 +1,20 @@
 import {getByText, render, screen} from "@testing-library/react"
 import Greet from "./Greet"
+describe("Greet", ()=>{
 
-test('Greet renders correctly',()=>{
-  render(<Greet/>)
- const textElement =  screen.getByText('Hello')
- expect(textElement).toBeInTheDocument()
+  test('renders correctly',()=>{
+    render(<Greet/>)
+    const textElement =  screen.getByText('Hello')
+    expect(textElement).toBeInTheDocument()
+  })
+  
+
 })
 
-test('Greet renders with a new name', ()=>{
-  render(<Greet name='Vishwas'/>)
-  const textElement = screen.getByText("Hello Vishwas")
-  expect(textElement).toBeInTheDocument()
-})
+describe('Nested', () => { 
+  test('renders with a new name', ()=>{
+    render(<Greet name='Vishwas'/>)
+    const textElement = screen.getByText("Hello Vishwas")
+    expect(textElement).toBeInTheDocument()
+  })
+ })
